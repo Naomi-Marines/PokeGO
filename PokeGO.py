f66cdc964e7ac10d2b0f27de354c25bf7db6eae5
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2017-12-24 19:15:09 -0600

    Updated Python version.

--- PokeGO.py
+++ PokeGO.py
@@ -8,9 +8,10 @@
 
 # Importamos paquetes:
 import matplotlib.pyplot as plt
+import scipy.stats as st
+import seaborn as sns
 import numpy as np
-import scipy.stats
-import scipy
+
 
 # Muestra de tiempos:
 tiempos = np.array([2.50, 4.93, 5.01, 1.11, 4.93, 3.51,
@@ -19,18 +20,40 @@ tiempos = np.array([2.50, 4.93, 5.01, 1.11, 4.93, 3.51,
                     1.25, 2.78, 0.66, 5.36, 0.05, 2.13,
                     0.25, 2.28, 5.88, 18.45])
 
+
 # PASO 1 - Creamos histograma.
-n, bins, patches = plt.hist(tiempos, 16, normed=True,
-                            facecolor='green', alpha=0.75)
-plt.show()
+breaks = 20
+plt.figure(1)
+plt.hist(tiempos, breaks, normed=True, facecolor='blue',
+         alpha=0.75, ec='white', label="Histograma de tiempos")
+plt.title("Histograma de tiempos")
+plt.xlabel("Tiempos")
+plt.ylabel("Frecuencia de tiempos")
+# plt.show()
+
 
 # PASO 2 - Proponemos un modelo.
+dist_name = 'dweibull'
+
 
 # PASO 3 - Estimamos parámetros.
 # Estimador de parámetros:
+dist = getattr(st, dist_name)
+param = dist.fit(tiempos)
+pdf_fitted = dist.pdf(tiempos, *param[:-2], loc=param[-2], scale=param[-1])
+pdf_fitted *= breaks
+plt.figure(2)
+plt.plot(pdf_fitted, label=dist_name)
+plt.title("Función de densidad ajustada")
+plt.xlim(0, breaks)
+print(param)
+# plt.show()
+
 
 # PASO 4 - Validamos nuestro modelo.
 # Función de Distribución Empírica (FDE):
+
+
 # Gráfica cuantil-cuantil:
 # Densidad sobre histograma:
 
